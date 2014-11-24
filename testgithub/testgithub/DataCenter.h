c000b58b98f81fa4ca4af1bb3ec047cb7570c4b3
jackytong <jackytong from polaristech>
2014-11-24 10:00:11 +0800

    this is my testing.

---
+++ testgithub/testgithub/DataCenter.h
@@ -0,0 +1,13 @@
+//
+//  DataCenter.h
+//  testgithub
+//
+//  Created by admin on 11/15/14.
+//  Copyright (c) 2014 Polaris Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataCenter : NSObject
+
+@end
---
+++ testgithub/testgithub/DataCenter.m
@@ -0,0 +1,13 @@
+//
+//  DataCenter.m
+//  testgithub
+//
+//  Created by admin on 11/15/14.
+//  Copyright (c) 2014 Polaris Technology. All rights reserved.
+//
+
+#import "DataCenter.h"
+
+@implementation DataCenter
+
+@end
